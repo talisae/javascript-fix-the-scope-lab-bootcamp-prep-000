5055b04864da6a9054ebf3e1101358167d3da528
Talisa Erazo <talisaerazo@gmail.com>
2018-09-08 23:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function myAnimal() {
   return animal
 }
 
-function Scope yourAnimal() {
+function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
