39dcb95f9aae092bfa4a29eebc60acce414c081a
Talisa Erazo <talisaerazo@gmail.com>
2018-09-08 23:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ return animal;
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+  var animal = 'cat';
+  return animal;
 }
 
 function add2(n) {
